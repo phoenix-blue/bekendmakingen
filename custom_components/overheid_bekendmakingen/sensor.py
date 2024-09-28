ed680703dd2cc0ad32229b5ebfd97821d655cf0b
phoenix-blue <phoenix-blue@users.noreply.github.com>
2024-09-28 21:10:20 +0200

    fix range

--- custom_components/overheid_bekendmakingen/sensor.py
+++ custom_components/overheid_bekendmakingen/sensor.py
@@ -159,7 +159,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
     name = entry.data.get("name", "Overheid bekendmakingen")
     latitude = entry.data.get("latitude", hass.config.latitude)
     longitude = entry.data.get("longitude", hass.config.longitude)
-    range_km = entry.data.get("range_km", 3)  # Default range in kilometers is now 3
+    range_km = entry.data.get("range_km", 3.0)  # Default range in kilometers is now 3
     interval = entry.data.get("update_interval_hours", 12) * 3600  # Convert to seconds
     debug = entry.data.get("debug", False)  # Get the debug option
 
