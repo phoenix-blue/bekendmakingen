2439b31806c0b0b38026d67096f3b108ca44c7ba
phoenix-blue <phoenix-blue@users.noreply.github.com>
2025-10-19 21:56:47 +0200

    Update version to 2.0.8

--- custom_components/overheid_bekendmakingen/const.py
+++ custom_components/overheid_bekendmakingen/const.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 DOMAIN = "overheid_bekendmakingen"
 NAME = "Overheid Bekendmakingen"
-VERSION = "2.0.7"  # Fixed update interval polling issue
+VERSION = "2.0.8"  # Proper HACS/HA directory structure
 
 # Configuratie sleutels
 CONF_LATITUDE = "latitude"
